93632321f216312b3b0bfad97ba4c80a60a2bc2b
Jim Publicover <jameslpublicover@gmail.com>
2017-02-13 15:14:36 -0500

    delete food link on entry/show up and running

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,21 @@
 class FoodsController < ApplicationController
 
+  # def show
+  #   @entry = Entry.find(params[:entry_id])
+  #   @food = @entry.foods.find(params[:id])
+  # end
+
   def create
     @entry = Entry.find(params[:entry_id])
     @food = @entry.foods.create(food_params)
+    redirect_to entry_path(@entry)
+  end
+
+  def destroy
+    @entry = Entry.find(params[:entry_id])
+    @food = @entry.foods.find(params[:id])
+    @food.destroy
+
     redirect_to entry_path(@entry)
   end
 
